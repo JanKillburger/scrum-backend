346165552d41b33fa7c3dbcf0e0d1a0f8da85ec4
Jan Killburger <jkillburger@gmail.com>
2024-04-21 15:26:11 +0200

    added subtask creation option in task update operation and added tests

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -3,7 +3,7 @@ from . import models
 
 
 class SubTaskSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(required=False)
 
     class Meta:
         model = models.SubTask
@@ -11,32 +11,50 @@ class SubTaskSerializer(serializers.ModelSerializer):
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    subtasks = SubTaskSerializer(many=True)
+    subtasks = SubTaskSerializer(many=True, required=False)
 
     class Meta:
         model = models.Task
         fields = "__all__"
 
     def create(self, validated_data):
-        subtasks_data = validated_data.pop("subtasks")
+        if "subtasks" in validated_data.keys():
+            subtasks_data = validated_data.pop("subtasks")
         task = models.Task.objects.create(**validated_data)
-        for subtask_data in subtasks_data:
-            models.SubTask.objects.create(task=task, **subtask_data)
+        if subtasks_data:
+            for subtask_data in subtasks_data:
+                models.SubTask.objects.create(task=task, **subtask_data)
         return task
 
     def update(self, instance, validated_data):
-        new_subtasks_data = validated_data.pop("subtasks")
-        self.handleSubtasks(instance, new_subtasks_data)
+        if "subtasks" in validated_data.keys():
+            subtasks_data = validated_data.pop("subtasks")
+            self.handle_subtasks(instance, subtasks_data)
         super().update(instance, validated_data)
         return instance
 
-    def handleSubtasks(self, task, validated_subtasks_data):
+    def handle_subtasks(self, task, validated_subtasks_data):
+        """Create, update and delete subtasks.
+        
+            Create new subtask if 'id' key is not provided.
+            Update subtasks for which 'id' key is provided.
+            Delete subtasks if they are not provided."""
         db_subtasks = task.subtasks.all()
-        request_ids = [subtask["id"] for subtask in validated_subtasks_data]
-        data = {subtask["id"]: subtask for subtask in validated_subtasks_data}
-        for subtask in db_subtasks:
-            if subtask.id not in request_ids:
-                subtask.delete()
+        items_to_update = {}
+        items_to_create = []
+        for validated_subtask_data in validated_subtasks_data:
+            if "id" in validated_subtask_data.keys():
+                items_to_update[validated_subtask_data["id"]] = validated_subtask_data
             else:
-                subtask_data = data[subtask.id]
-                models.SubTask.objects.filter(id=subtask.id).update(**subtask_data)
+                items_to_create.append(validated_subtask_data)
+
+        for subtask in db_subtasks:
+            if subtask.id in items_to_update.keys():
+                models.SubTask.objects.filter(id=subtask.id).update(
+                    **items_to_update[subtask.id]
+                )
+            else:
+                subtask.delete()
+
+        for subtask in items_to_create:
+            models.SubTask.objects.create(task=task, **subtask)
--- scrumboard/tests.py
+++ scrumboard/tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
 from rest_framework.authtoken.models import Token
-from scrumboard.models import Task
+from scrumboard.models import SubTask, Task
 
+def get_authenticated_client():
+    user = User.objects.create_user("testuser")
+    token = Token.objects.create(user=user)
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Token  {token.key}")
+    return client
 
 class TaskModelTests(TestCase):
     def test_default_open_status(self):
@@ -18,16 +25,66 @@ class TaskModelTests(TestCase):
         self.assertIs(task.status, "open")
 
 
-class TaskViewTests(TestCase):
+class TaskViewTests(APITestCase):
     def test_unauthenticated_user(self):
         client = APIClient()
         response = client.get("/tasks/")
-        self.assertIs(response.status_code, 401)
+        self.assertEqual(response.status_code, 401)
 
     def test_authenticated_user(self):
-        user = User.objects.create_user("testuser")
-        token = Token.objects.create(user=user)
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f"Token  {token.key}")
+        client = get_authenticated_client()
         response = client.get("/tasks/")
-        self.assertIs(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+
+    def test_subtask_create_update_delete(self):
+        client = get_authenticated_client()
+        create_response = client.post(
+            "/tasks/",
+            {
+                "title": "Test title",
+                "description": "Test desc",
+                "due_date": "2024-04-28",
+                "assigned_to": 1,
+                "subtasks": [
+                    {
+                        "title": "first subtask",
+                        "completed": False
+                    }
+                ]
+            },
+            format="json"
+        )
+        self.assertEqual(create_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SubTask.objects.count(), 1)
+        update_response = client.put(
+            "/tasks/1/",
+            {
+                "title": "Test title",
+                "description": "Test desc",
+                "due_date": "2024-04-28",
+                "assigned_to": 1,
+                "subtasks": [
+                    {
+                        "id": 1,
+                        "title": "first subtask",
+                        "completed": True
+                    }
+                ]
+            },
+            format="json"
+        )
+        self.assertEqual(update_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(SubTask.objects.get(pk=1).completed, True)
+        delete_response = client.put(
+            "/tasks/1/",
+            {
+                "title": "Test title",
+                "description": "Test desc",
+                "due_date": "2024-04-28",
+                "assigned_to": 1,
+                "subtasks": []
+            },
+            format="json"
+        )
+        self.assertEqual(delete_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(SubTask.objects.count(), 0)
