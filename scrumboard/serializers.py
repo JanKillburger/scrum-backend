e31f27c83ee015ffd9cff1edc575b80fc9ef46e2
Jan Killburger <jkillburger@gmail.com>
2024-04-28 13:52:18 +0200

    fixed permissions

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -10,6 +10,7 @@ class RegisterUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+
         return user
 
 class UserSerializer(serializers.ModelSerializer):
--- scrumboard/views.py
+++ scrumboard/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from scrumboard.models import Task
 from scrumboard.permissions import IsOwnerOrAssigneeOrReadonly, IsUserOrReadonly
 from scrumboard.serializers import RegisterUserSerializer, TaskSerializer, UserSerializer
@@ -9,7 +10,7 @@ from scrumboard.serializers import RegisterUserSerializer, TaskSerializer, UserS
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
-    permission_classes = [IsOwnerOrAssigneeOrReadonly]
+    permission_classes = [IsOwnerOrAssigneeOrReadonly, IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -17,7 +18,7 @@ class TaskViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsUserOrReadonly]
+    permission_classes = [IsUserOrReadonly, IsAuthenticated]
 
 class RegisterUser(generics.CreateAPIView):
     serializer_class = RegisterUserSerializer
